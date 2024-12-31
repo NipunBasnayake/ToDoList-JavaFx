f7db3d90cdf8f164191ef307b9390e263635e4cb
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2025-01-01 00:45:47 +0530

    Login view

--- src/main/java/controller/LoginViewController.java
+++ src/main/java/controller/LoginViewController.java
@@ -1,4 +1,63 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+
 public class LoginViewController {
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+        try {
+            boolean isExist = authenticateUsernamePassword(txtUserName.getText(), txtPassword.getText());
+            if (isExist) {
+                Stage stage = new Stage();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/todo-view.fxml"))));
+                stage.show();
+
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Login");
+                alert.setHeaderText(null);
+                alert.setContentText("Login Unsuccessful");
+                alert.show();
+            }
+        } catch (SQLException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Error");
+            alert.setHeaderText(null);
+            alert.setContentText("An error occurred while processing your request. Please try again later.");
+            alert.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean authenticateUsernamePassword(String userName, String password) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+        preparedStatement.setString(1, userName);
+        preparedStatement.setString(2, password);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            return true;
+        }
+        return false;
+    }
+
 }
